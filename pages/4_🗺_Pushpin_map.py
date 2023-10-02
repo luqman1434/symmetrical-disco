c4ac0c235d64c33ea75246ec7a0d11caa3859130
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 10:58:50 +0800

    Update 4_🗺_Pushpin_map.py

--- pages/4_🗺_Pushpin_map.py
+++ pages/4_🗺_Pushpin_map.py
@@ -4,10 +4,10 @@ import pandas as pd
 import altair as alt
 from urllib.error import URLError
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="🗺")
+st.set_page_config(page_title="Pushpin Map", page_icon="🗺")
 
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
+st.markdown("# Pushpin Map Demo")
+st.sidebar.header("Pushpin Map")
 # st.write(
 #     """This demo shows how to use `st.write` to visualize Pandas DataFrames.
 # (Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
