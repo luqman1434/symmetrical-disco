d28b373df937a4ae587724c8571af04e47b14776
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 10:49:02 +0800

    Update 4_🗺_Pushpin_map.py

--- pages/4_🗺_Pushpin_map.py
+++ pages/4_🗺_Pushpin_map.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
-def main():
-    st.title("Hello, World!")
-    st.write("Welcome to your Streamlit app.")
+# def main():
+#     st.title("Hello, World!")
+#     st.write("Welcome to your Streamlit app.")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+# def main():
+st.title("Hello, World!")
+st.write("Welcome to your Streamlit app.")
+
+# if __name__ == "__main__":
+#     main()
