3e2da3bb0a187f0fa5550021a22237021448400b
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 15:25:43 +0800

    Update 5_🗿_OriginalMap_Demo.py

--- pages/5_🗿_OriginalMap_Demo.py
+++ pages/5_🗿_OriginalMap_Demo.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
 import os
 
 st.set_page_config(page_title="Testing", page_icon="🗿")
@@ -17,7 +18,13 @@ html_file = pd.read_html(html_file_path)
 
 # msia_district.geojson
 geojson_file_path = os.path.join(parent_dir, 'msia_district.geojson')
-geojson_file = pd.read_excel(geojson_file_path)
+# geojson_file = pd.read_excel(geojson_file_path)
+geojson_file = gpd.read_file(geojson_file_path)
+
+# geojson_file = "msia_district.geojson"
+# with open(geojson_file) as gj_f:
+#     geojson_data = gpd.read_file(gj_f)
+
 
 st.write(data.columns.tolist())
 
