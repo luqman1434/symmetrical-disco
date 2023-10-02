b66b81d1f19f0e8f0e2408259c673cfec663bced
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 12:39:21 +0800

    Update 0_😓_testing new stuff.py

--- pages/0_😓_testing new stuff.py
+++ pages/0_😓_testing new stuff.py
@@ -1,5 +1,19 @@
+# import streamlit as st
+# import pandas as pd
+
+# st.set_page_config(page_title="Testing", page_icon="😓")
+# st.markdown("# Testing Demo")
+
 import streamlit as st
 import pandas as pd
+from PIL import Image
+
+# Define the path to the sample.png image
+sample_image_path = '../Screenshot_360.jpg'  # Assuming sample.png is located in the parent directory
+
+# Display the image
+sample_image = Image.open(sample_image_path)
+st.image(sample_image, caption='Sample Image', use_column_width=True)
 
 st.set_page_config(page_title="Testing", page_icon="😓")
 st.markdown("# Testing Demo")
