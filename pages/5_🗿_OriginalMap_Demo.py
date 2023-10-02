e6388e34ef9f0df8737cce5c4ff331bfc675c1a8
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 15:52:21 +0800

    Update 5_🗿_OriginalMap_Demo.py

--- pages/5_🗿_OriginalMap_Demo.py
+++ pages/5_🗿_OriginalMap_Demo.py
@@ -13,22 +13,22 @@ st.markdown("# Testing Demo")
 # Get the parent directory of the script's directory
 parent_dir = os.path.dirname(os.path.dirname(__file__))
 
-# # MMU ITP List 13_9_9_11.xlsx
-# excel_file_path = os.path.join(parent_dir, 'MMU ITP List 13_9_9_11.xlsx')
-# xlsx = pd.read_excel(excel_file_path)
+# MMU ITP List 13_9_9_11.xlsx
+excel_file_path = os.path.join(parent_dir, 'MMU ITP List 13_9_9_11.xlsx')
+xlsx = pd.read_excel(excel_file_path)
 
 # itp_area_map.html
 html_file_path = os.path.join(parent_dir, 'itp_area_map.html')
 # html_file = pd.read_html(html_file_path)
 with open(html_file_path, 'r') as f:
-    html_content = f.read()
-    st.markdown(html_content, unsafe_allow_html=True)
+    html_file = f.read()
+    # st.markdown(html_content, unsafe_allow_html=True)
 
-# # msia_district.geojson
-# geojson_file_path = os.path.join(parent_dir, 'msia_district.geojson')
-# geojson_file = gpd.read_file(geojson_file_path)
+# msia_district.geojson
+geojson_file_path = os.path.join(parent_dir, 'msia_district.geojson')
+geojson_file = gpd.read_file(geojson_file_path)
 
 
-# # Show the column names
-# st.markdown("### Column Names:")
-# st.write(xlsx.columns.tolist())
+# Show the column names
+st.markdown("### Column Names:")
+st.write(xlsx.columns.tolist())
