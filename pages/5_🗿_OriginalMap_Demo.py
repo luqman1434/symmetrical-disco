59a59f723a61b69c5f18dbc09f75530cd4db2e66
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 15:43:25 +0800

    Update 5_🗿_OriginalMap_Demo.py

--- pages/5_🗿_OriginalMap_Demo.py
+++ pages/5_🗿_OriginalMap_Demo.py
@@ -10,14 +10,16 @@ st.set_page_config(page_title="Testing", page_icon="🗿")
 st.markdown("# Testing Demo")
 
 
-# # Get the parent directory of the script's directory
+# Get the parent directory of the script's directory
 parent_dir = os.path.dirname(os.path.dirname(__file__))
-# excel_file_path = os.path.join(parent_dir, 'MMU ITP List 13_9_9_11.xlsx')
-# xlsx = pd.read_excel(excel_file_path)
 
-# # itp_area_map.html
-# html_file_path = os.path.join(parent_dir, 'itp_area_map.html')
-# html_file = pd.read_html(html_file_path)
+# MMU ITP List 13_9_9_11.xlsx
+excel_file_path = os.path.join(parent_dir, 'MMU ITP List 13_9_9_11.xlsx')
+xlsx = pd.read_excel(excel_file_path)
+
+# itp_area_map.html
+html_file_path = os.path.join(parent_dir, 'itp_area_map.html')
+html_file = pd.read_html(html_file_path)
 
 # msia_district.geojson
 geojson_file_path = os.path.join(parent_dir, 'msia_district.geojson')
