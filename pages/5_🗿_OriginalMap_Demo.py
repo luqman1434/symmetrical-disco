cc17cd06f13a56ab3dcd6f642e9266bc1cac67b2
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 15:43:46 +0800

    Update 5_🗿_OriginalMap_Demo.py

--- pages/5_🗿_OriginalMap_Demo.py
+++ pages/5_🗿_OriginalMap_Demo.py
@@ -26,4 +26,4 @@ geojson_file_path = os.path.join(parent_dir, 'msia_district.geojson')
 geojson_file = gpd.read_file(geojson_file_path)
 
 
-# st.write(xlsx.columns.tolist())
+st.write(xlsx.columns.tolist())
