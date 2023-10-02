33e99e538274c68fb5a3ed9527c850b88ae587cb
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-02 13:48:54 +0800

    Update 5_🗿_OriginalMap_Demo.py

--- pages/5_🗿_OriginalMap_Demo.py
+++ pages/5_🗿_OriginalMap_Demo.py
@@ -5,3 +5,11 @@ import os
 
 st.set_page_config(page_title="Testing", page_icon="🗿")
 st.markdown("# Testing Demo")
+
+
+# Get the parent directory of the script's directory
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+excel_file_path = os.path.join(parent_dir, 'MMU ITP List 13_9_9_11.xlsx')
+data = pd.read_excel(excel_file_path)
+
+st.write(data.columns.tolist())
