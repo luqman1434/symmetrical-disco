8f9a20d3d0c9d9ad6ab6a1018c6df1ab524cccf0
kenwoo29 <78064242+devInTrainin@users.noreply.github.com>
2023-10-05 13:08:22 +0800

    Update 7_📊_2nd Graph Demo.py

--- pages/7_📊_2nd Graph Demo.py
+++ pages/7_📊_2nd Graph Demo.py
@@ -18,30 +18,38 @@ except FileNotFoundError:
 st.title("Pie Chart Filter")
 
 # Sidebar
-state_filter = st.sidebar.selectbox("Filter by State", data['STATE'].unique())
+# state_filter = st.sidebar.selectbox("Filter by State", data['STATE'].unique())
+state_filter = st.sidebar.selectbox("Filter by State", df['STATE'].unique())
+
 
 # Filter data based on state selection
-filtered_data = data if state_filter == 'All' else data[data['STATE'] == state_filter]
+# filtered_data = data if state_filter == 'All' else data[data['STATE'] == state_filter]
+filtered_data = df if state_filter == 'All' else df[df['STATE'] == state_filter]
+
 
 # Display overall state pie chart
 st.subheader("Overall State Distribution")
-state_counts = data['STATE'].value_counts()  # Count all states, not just filtered ones
+# state_counts = data['STATE'].value_counts()  # Count all states, not just filtered ones
+state_counts = df['STATE'].value_counts()  # Count all states, not just filtered ones
 state_pie_fig = px.pie(names=state_counts.index, values=state_counts.values)
 st.plotly_chart(state_pie_fig)
 
 # Filter by city within the selected state
 if state_filter != 'All':
-    city_filter = st.sidebar.selectbox("Filter by City", data[data['STATE'] == state_filter]['CITY'].unique())
+    # city_filter = st.sidebar.selectbox("Filter by City", data[data['STATE'] == state_filter]['CITY'].unique())
+    city_filter = st.sidebar.selectbox("Filter by City", df[df['STATE'] == state_filter]['CITY'].unique())
     st.subheader(f"City Distribution in {state_filter}")
     
     # Count all cities for the selected state from the original dataset
-    city_counts = data[data['STATE'] == state_filter]['CITY'].value_counts()
+    # city_counts = data[data['STATE'] == state_filter]['CITY'].value_counts()
+    city_counts = df[df['STATE'] == state_filter]['CITY'].value_counts()
     city_pie_fig = px.pie(names=city_counts.index, values=city_counts.values)
     st.plotly_chart(city_pie_fig)
 
     # Filter by address within the selected city
     if city_filter != 'All':
         st.subheader(f"Address Distribution in {city_filter}")
-        address_counts = data[(data['STATE'] == state_filter) & (data['CITY'] == city_filter)]['ADDRESS'].value_counts()
+        # address_counts = data[(data['STATE'] == state_filter) & (data['CITY'] == city_filter)]['ADDRESS'].value_counts()
+        address_counts = df[(df['STATE'] == state_filter) & (df['CITY'] == city_filter)]['ADDRESS'].value_counts()
         address_pie_fig = px.pie(names=address_counts.index, values=address_counts.values)
         st.plotly_chart(address_pie_fig)
