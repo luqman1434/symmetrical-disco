ad06b8d8d070ca2d7d5810fa0ffb4fbab3f88b07
luqman1434 <145521860+luqman1434@users.noreply.github.com>
2023-10-11 10:29:11 +0800

    Update 9_🗿_OriginalMap_Demo.py

--- pages/9_🗿_OriginalMap_Demo.py
+++ pages/9_🗿_OriginalMap_Demo.py
@@ -1,69 +1,118 @@
 import streamlit as st
 import pandas as pd
 import geopandas as gpd
-import folium
+import os
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+st.set_page_config(page_title="2nd Graph Choropleth Demo", page_icon="🗺")
+st.markdown("# 2nd Graph Choropleth Demo")
+
+st.sidebar.header("2nd Graph Choropleth Demo Demo")
+st.write(
+    """This version of the map shows how the map works with two different filters. One of the filters toggle the choropleth mask on/off on the filter. Another one of the filters toggle on the activation of the 
+    pushpins so we can focus on specific filters inside the map."""
+)
+
 import math
+import json
+import warnings
+
+import pandas as pd
+import geopandas as gpd
+import folium
+from branca.element import Figure
 from shapely.geometry import Point
+
+import streamlit as st
+import streamlit.components.v1 as components
 from streamlit_folium import st_folium
 
-# Initialization of Streamlit's session state variable
-if 'sidebar_data' not in st.session_state:
-    st.session_state.sidebar_data = ''
-
+# Define the function to read the Excel file
 def read_file(filename, sheetname):
-    """Function to read the Excel file."""
     excel_file = pd.ExcelFile(filename)
     data_d = excel_file.parse(sheet_name=sheetname)
     return data_d
 
-def plot_markers(map_obj, data):
-    """Function to plot markers on the map."""
-    for itp_data in data.to_dict(orient='records'):
+def plot_choropleth(map_obj, show_choropleth=True):
+    if show_choropleth:
+        choropleth = folium.Choropleth(
+            geo_data=merged_gdf,
+            name='choropleth',
+            data=merged_gdf,
+            columns=['NAME_2', 'count'],
+            key_on='feature.properties.NAME_2',
+            fill_color='RdYlGn',
+            fill_opacity=0.7,
+            line_opacity=0.2,
+            threshold_scale=threshold_scale,
+            line_color='black',
+            legend_name='District Counts',
+            highlight=False
+        ).add_to(map_obj)
+        folium.GeoJsonTooltip(fields=['NAME_1','NAME_2', 'count'], aliases=['State','District', 'Count']).add_to(choropleth.geojson)
+
+if __name__ == '__main__':
+    st.title('Available ITP companies in Malaysia')
+
+    file_input = 'MMU ITP List 13_9_9_11.xlsx'
+    geojson_file = "msia_district.geojson"
+
+    text_load_state = st.text('Reading files ...')
+    with open(geojson_file, encoding='utf-8', errors='ignore') as gj_f:
+        geojson_data = gpd.read_file(gj_f)
+
+    itp_list_state = read_file(file_input, 0)
+    text_load_state.text('Reading files ... Done!') 
+
+    map_size = Figure(width=800, height=600)
+    map_my = folium.Map(location=[4.2105, 108.9758], zoom_start=6)
+    map_size.add_child(map_my)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+
+    itp_list_state['geometry'] = itp_list_state.apply(lambda x: Point(x['map_longitude'], x['map_latitude']), axis=1)
+    itp_list_state = gpd.GeoDataFrame(itp_list_state, geometry='geometry')
+
+    selected_states = st.multiselect('Select States',itp_list_state['STATE'].unique())
+    
+    filtered_data = itp_list_state[itp_list_state['STATE'].isin(selected_states)]
+
+    joined_data = gpd.sjoin(geojson_data, filtered_data, op="contains").groupby(["NAME_1", "NAME_2"]).size().reset_index(name="count")
+
+    merged_gdf = geojson_data.merge(joined_data, on=["NAME_1", "NAME_2"], how="left")
+    merged_gdf['count'].fillna(0, inplace=True)
+
+    threshold_scale = [0, 1, 2, 4, 8, 16, 32, 64, 128, 200, 300, 400] 
+
+    text_load_state.text('Plotting ...')
+    for itp_data in filtered_data.to_dict(orient='records'):
         latitude = itp_data['map_latitude']
         longitude = itp_data['map_longitude']
         company_name = itp_data['Company name']
-        data_str = company_name + ': ' + itp_data['Company address']
-        popup_content = f"""
-        <div>
-            {company_name}<br>
-            <a href="/?sidebar_data={data_str}">Show in sidebar</a>
-        </div>
-        """
+        popup_name = '<strong>' + str(itp_data['Company name']) + '</strong>\n' + str(itp_data['Company address'])
         if not math.isnan(latitude) and not math.isnan(longitude):
-            folium.Marker(location=[latitude, longitude], popup=popup_content, tooltip=company_name).add_to(map_obj)
+            details_html = f"<h3>{company_name}</h3><p>{itp_data['Company address']}</p>"
+            marker = folium.Marker(location=[latitude, longitude], popup=popup_name, tooltip=company_name)
 
-st.title('Available ITP companies in Malaysia')
+            # Define a custom click event handler to update the sidebar
+            js_click_handler = f"""
+                function onClick() {{
+                    parent.postMessage({{ value: '{details_html}' }}, '*');
+                }}
+                this.on('click', onClick);
+            """
+            marker.add_child(folium.CustomPane('clickable')).add_to(map_my)
+            map_my.get_root().html.add_child(folium.Element(js_click_handler))
 
-file_input = 'MMU ITP List 13_9_9_11.xlsx'
-geojson_file = "msia_district.geojson"
+    text_load_state.text('Plotting ... Done!')
+    
+    show_choropleth = st.checkbox("Show Choropleth", value=False)
+    if show_choropleth:
+        plot_choropleth(map_my)
 
-# Reading geojson and data
-with open(geojson_file, encoding='utf-8', errors='ignore') as gj_f:
-    geojson_data = gpd.read_file(gj_f)
-itp_list_state = read_file(file_input, 0)
-
-map_my = folium.Map(location=[4.2105, 108.9758], zoom_start=6)
-
-itp_list_state['geometry'] = itp_list_state.apply(lambda x: Point(x['map_longitude'], x['map_latitude']), axis=1)
-itp_list_state = gpd.GeoDataFrame(itp_list_state, geometry='geometry')
-
-selected_states = st.multiselect('Select States', itp_list_state['STATE'].unique())
-filtered_data = itp_list_state[itp_list_state['STATE'].isin(selected_states)]
-
-# Plot markers on the map
-plot_markers(map_my, filtered_data)
-
-show_choropleth = st.checkbox("Show Choropleth", value=False)
-if show_choropleth:
-    plot_choropleth(map_my) # Assuming this function remains the same as in your initial code
-
-st_folium(map_my)
-
-# Display sidebar data
-if st.session_state.sidebar_data:
-    st.sidebar.text(st.session_state.sidebar_data)
-
-# Update sidebar data if received from the map
-sidebar_data = st.experimental_get_query_params().get('sidebar_data')
-if sidebar_data:
-    st.session_state.sidebar_data = sidebar_data[0]
+    map_my.save('itp_area_map.html')
+    p = open('itp_area_map.html')
+    components.html(p.read(), 800, 480)
