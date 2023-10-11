d1f1afbfb53b178bdc6d709169a3b5b4b621d023
luqman1434 <145521860+luqman1434@users.noreply.github.com>
2023-10-11 20:16:26 +0800

    Update 9_🗿_OriginalMap_Demo.py

--- pages/9_🗿_OriginalMap_Demo.py
+++ pages/9_🗿_OriginalMap_Demo.py
@@ -1,1 +1,28 @@
+# app.py
+import streamlit as st
+import pandas as pd
+
+# Load the dataset
+@st.cache
+def load_data():
+    # Assuming the Excel file is named "companies.xlsx" and the sheet name is "Sheet1"
+    data = pd.read_excel("companies.xlsx", engine="openpyxl")
+    return data
+
+df = load_data()
+
+# Streamlit App UI
+st.title('Company Search App')
+
+# Text Input for Company Name Search
+search_term = st.text_input("Enter Company Name:")
+
+if search_term:
+    # Filter dataframe based on user input
+    results = df[df['Company'].str.contains(search_term, case=False, na=False)]
+
+    # Display results
+    st.write(results)
+
+# Run this by typing 'streamlit run app.py' in the terminal
 
