76c4ac2d5d62fcf5c78fdbde46d8344d352a1721
luqman1434 <145521860+luqman1434@users.noreply.github.com>
2023-10-11 10:13:45 +0800

    Update 9_🗿_OriginalMap_Demo.py

--- pages/9_🗿_OriginalMap_Demo.py
+++ pages/9_🗿_OriginalMap_Demo.py
@@ -3,17 +3,21 @@ import pandas as pd
 import geopandas as gpd
 import folium
 import math
-import streamlit.components.v1 as components
 from shapely.geometry import Point
 from streamlit_folium import st_folium
 
-# Define the function to read the Excel file
+# Initialization of Streamlit's session state variable
+if 'sidebar_data' not in st.session_state:
+    st.session_state.sidebar_data = ''
+
 def read_file(filename, sheetname):
+    """Function to read the Excel file."""
     excel_file = pd.ExcelFile(filename)
     data_d = excel_file.parse(sheet_name=sheetname)
     return data_d
 
 def plot_markers(map_obj, data):
+    """Function to plot markers on the map."""
     for itp_data in data.to_dict(orient='records'):
         latitude = itp_data['map_latitude']
         longitude = itp_data['map_longitude']
@@ -22,70 +26,44 @@ def plot_markers(map_obj, data):
         popup_content = f"""
         <div>
             {company_name}<br>
-            <a href="#" onclick="updateStreamlit('{data_str}'); return false;">Show in sidebar</a>
+            <a href="/?sidebar_data={data_str}">Show in sidebar</a>
         </div>
         """
         if not math.isnan(latitude) and not math.isnan(longitude):
             folium.Marker(location=[latitude, longitude], popup=popup_content, tooltip=company_name).add_to(map_obj)
 
-# JavaScript to communicate with Streamlit
-js = """
-<script>
-    function updateStreamlit(data) {
-        const Http = new XMLHttpRequest();
-        const url = '/update_data?data=' + encodeURIComponent(data);
-        Http.open("GET", url);
-        Http.send();
-
-        Http.onreadystatechange = (e) => {
-            if(Http.readyState === 4 && Http.status === 200) {
-                window.location.reload();
-            }
-        }
-    }
-</script>
-"""
-
-# ... Rest of your initial setup ...
-
 st.title('Available ITP companies in Malaysia')
-st.markdown(js, unsafe_allow_html=True) # Add the JS here
 
-# ... Loading data ...
+file_input = 'MMU ITP List 13_9_9_11.xlsx'
+geojson_file = "msia_district.geojson"
 
-map_size = Figure(width=800, height=600)
+# Reading geojson and data
+with open(geojson_file, encoding='utf-8', errors='ignore') as gj_f:
+    geojson_data = gpd.read_file(gj_f)
+itp_list_state = read_file(file_input, 0)
+
 map_my = folium.Map(location=[4.2105, 108.9758], zoom_start=6)
-map_size.add_child(map_my)
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
 
 itp_list_state['geometry'] = itp_list_state.apply(lambda x: Point(x['map_longitude'], x['map_latitude']), axis=1)
 itp_list_state = gpd.GeoDataFrame(itp_list_state, geometry='geometry')
 
 selected_states = st.multiselect('Select States', itp_list_state['STATE'].unique())
-
 filtered_data = itp_list_state[itp_list_state['STATE'].isin(selected_states)]
-
-joined_data = gpd.sjoin(geojson_data, filtered_data, op="contains").groupby(["NAME_1", "NAME_2"]).size().reset_index(name="count")
-
-merged_gdf = geojson_data.merge(joined_data, on=["NAME_1", "NAME_2"], how="left")
-merged_gdf['count'].fillna(0, inplace=True)
-
-threshold_scale = [0, 1, 2, 4, 8, 16, 32, 64, 128, 200, 300, 400] 
 
 # Plot markers on the map
 plot_markers(map_my, filtered_data)
 
 show_choropleth = st.checkbox("Show Choropleth", value=False)
 if show_choropleth:
-    plot_choropleth(map_my, show_choropleth)
+    plot_choropleth(map_my) # Assuming this function remains the same as in your initial code
 
-map_my.save('itp_area_map.html')
-p = open('itp_area_map.html')
-components.html(p.read(), 800, 480)
+st_folium(map_my)
 
-# Handle data from the JS function
-if st.experimental_get_query_params().get('data'):
-    data = st.experimental_get_query_params()['data'][0]
-    st.sidebar.text(data)
+# Display sidebar data
+if st.session_state.sidebar_data:
+    st.sidebar.text(st.session_state.sidebar_data)
+
+# Update sidebar data if received from the map
+sidebar_data = st.experimental_get_query_params().get('sidebar_data')
+if sidebar_data:
+    st.session_state.sidebar_data = sidebar_data[0]
