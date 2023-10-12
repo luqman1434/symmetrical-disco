06cad5ad95ffda21a7df6ca105c32323f7b0ca21
luqman1434 <145521860+luqman1434@users.noreply.github.com>
2023-10-12 08:28:04 +0800

    Update 9_🗿_OriginalMap_Demo.py

--- pages/9_🗿_OriginalMap_Demo.py
+++ pages/9_🗿_OriginalMap_Demo.py
@@ -1,4 +1,3 @@
-# app.py
 import streamlit as st
 import pandas as pd
 from html import escape
@@ -6,7 +5,6 @@ from html import escape
 # Load the dataset
 @st.cache
 def load_data():
-    # Use the specified Excel file name
     data = pd.read_excel("MMU ITP List 13_9_9_11.xlsx", engine="openpyxl")
     return data
 
@@ -42,9 +40,6 @@ else:
 # Text Input for Company Name Search above the table
 search_term = st.text_input("Enter Company Name:")
 
-# Define columns to display
-columns_to_display = ["Company name", "Company address", "website_url", "Company Tel", "Company Email"]
-
 # Filter by search term and selected states
 if search_term:
     filtered_df = df[df['Company name'].str.contains(search_term, case=False, na=False)]
@@ -55,13 +50,21 @@ filtered_df = filtered_df[filtered_df['STATE'].isin(selected_states)]
 
 # Define a function to create an HTML card for each company
 def create_card(row):
-    # Conditional components based on NaN values and escape the data
-    company_name = f"<h4>{escape(row['Company name'])}</h4>" if not pd.isna(row['Company name']) else ""
-    company_address = f"<p>{escape(row['Company address'])}</p>" if not pd.isna(row['Company address']) else ""
-    website_url = f"<p><a href='{escape(row['website_url'])}' target='_blank'>{escape(row['website_url'])}</a></p>" if not pd.isna(row['website_url']) else ""
-    company_tel = f"<p>{escape(row['Company Tel'])}</p>" if not pd.isna(row['Company Tel']) else ""
-    company_email = f"<p>{escape(row['Company Email'])}</p>" if not pd.isna(row['Company Email']) else ""
-    
+    company_name_data = str(row['Company name']).strip("'")
+    company_name = f"<h4>{escape(company_name_data)}</h4>" if not pd.isna(row['Company name']) else ""
+
+    company_address_data = str(row['Company address']).strip("'")
+    company_address = f"<p>{escape(company_address_data)}</p>" if not pd.isna(row['Company address']) else ""
+
+    website_url_data = str(row['website_url']).strip("'")
+    website_url = f"<p><a href='{escape(website_url_data)}' target='_blank'>{escape(website_url_data)}</a></p>" if not pd.isna(row['website_url']) else ""
+
+    company_tel_data = str(row['Company Tel']).strip("'")
+    company_tel = f"<p>{escape(company_tel_data)}</p>" if not pd.isna(row['Company Tel']) else ""
+
+    company_email_data = str(row['Company Email']).strip("'")
+    company_email = f"<p>{escape(company_email_data)}</p>" if not pd.isna(row['Company Email']) else ""
+
     card = f"""
     <div style="border:1px solid #eee; border-radius:5px; padding:10px; margin:5px; width: 30%; height: 300px; overflow: auto; display:inline-block; vertical-align:top">
         {company_name}
@@ -73,10 +76,8 @@ def create_card(row):
     """
     return card
 
-
-
 # Convert the filtered DataFrame to HTML cards
-cards = filtered_df[columns_to_display].apply(create_card, axis=1).tolist()
+cards = filtered_df.apply(create_card, axis=1).tolist()
 
 for i in range(0, len(cards), 3):
     row_cards = ''.join(cards[i:i+3])
