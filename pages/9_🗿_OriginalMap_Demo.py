a4eaca3da769cc7ab2afd1b94c7c0bf702d03db4
luqman1434 <145521860+luqman1434@users.noreply.github.com>
2023-10-12 08:20:27 +0800

    Update 9_🗿_OriginalMap_Demo.py

--- pages/9_🗿_OriginalMap_Demo.py
+++ pages/9_🗿_OriginalMap_Demo.py
@@ -1,7 +1,7 @@
 # app.py
 import streamlit as st
 import pandas as pd
-import re
+from html import escape
 
 # Load the dataset
 @st.cache
@@ -53,22 +53,14 @@ else:
 
 filtered_df = filtered_df[filtered_df['STATE'].isin(selected_states)]
 
-# Define a function to clean HTML tags from text
-def clean_html_tags(text):
-    # First, convert any HTML encoded characters to their actual representation
-    text_decoded = html.unescape(text)    
-    # Next, strip any existing HTML tags from the string
-    clean = re.compile('<.*?>')
-    return re.sub(clean, '', text_decoded)
-
 # Define a function to create an HTML card for each company
 def create_card(row):
-    # Conditional components based on NaN values and clean the data from HTML tags
-    company_name = f"<h4>{clean_html_tags(row['Company name'])}</h4>" if not pd.isna(row['Company name']) else ""
-    company_address = f"<p>{clean_html_tags(row['Company address'])}</p>" if not pd.isna(row['Company address']) else ""
-    website_url = f"<p><a href='{clean_html_tags(row['website_url'])}' target='_blank'>{clean_html_tags(row['website_url'])}</a></p>" if not pd.isna(row['website_url']) else ""
-    company_tel = f"<p>{clean_html_tags(row['Company Tel'])}</p>" if not pd.isna(row['Company Tel']) else ""
-    company_email = f"<p>{clean_html_tags(row['Company Email'])}</p>" if not pd.isna(row['Company Email']) else ""
+    # Conditional components based on NaN values and escape the data
+    company_name = f"<h4>{escape(row['Company name'])}</h4>" if not pd.isna(row['Company name']) else ""
+    company_address = f"<p>{escape(row['Company address'])}</p>" if not pd.isna(row['Company address']) else ""
+    website_url = f"<p><a href='{escape(row['website_url'])}' target='_blank'>{escape(row['website_url'])}</a></p>" if not pd.isna(row['website_url']) else ""
+    company_tel = f"<p>{escape(row['Company Tel'])}</p>" if not pd.isna(row['Company Tel']) else ""
+    company_email = f"<p>{escape(row['Company Email'])}</p>" if not pd.isna(row['Company Email']) else ""
     
     card = f"""
     <div style="border:1px solid #eee; border-radius:5px; padding:10px; margin:5px; width: 30%; height: 300px; overflow: auto; display:inline-block; vertical-align:top">
@@ -81,10 +73,11 @@ def create_card(row):
     """
     return card
 
+
+
 # Convert the filtered DataFrame to HTML cards
 cards = filtered_df[columns_to_display].apply(create_card, axis=1).tolist()
 
-# Display the cards in chunks of 3
 for i in range(0, len(cards), 3):
     row_cards = ''.join(cards[i:i+3])
     st.markdown(row_cards, unsafe_allow_html=True)
