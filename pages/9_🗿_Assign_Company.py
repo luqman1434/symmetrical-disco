4faa135028db3fa1ec4a761e90ff6dc6a62584ed
luqman1434 <145521860+luqman1434@users.noreply.github.com>
2023-10-19 20:07:14 +0800

    Update 9_🗿_Assign_Company.py

--- pages/9_🗿_Assign_Company.py
+++ pages/9_🗿_Assign_Company.py
@@ -1,93 +1,46 @@
-# app.py
-import streamlit as st
+import math
 import pandas as pd
-from html import escape
+import streamlit as st
 
-# Load the dataset
-@st.cache
-def load_data():
-    # Use the specified Excel file name
-    data = pd.read_excel("MMU ITP List 13_9_9_11.xlsx", engine="openpyxl")
-    return data
+# Load the Excel file
+itp_file = pd.ExcelFile('output_files/FACULTY_ASSIGNED_SPEC.xlsx')
+itp_df = itp_file.parse(sheet_name=0)
 
-df = load_data()
+# Streamlit UI
+st.title('Find Nearest Companies')
 
-# Streamlit App UI
-st.title('Assigning Company')
+# User input for coordinates
+st.sidebar.header('Input Coordinates')
+x = st.sidebar.number_input('X Coordinate:', min_value=-90.0, max_value=90.0, step=0.0001, format='%.7f')
+y = st.sidebar.number_input('Y Coordinate:', min_value=-180.0, max_value=180.0, step=0.0001, format='%.7f')
 
-# Custom styles to expand table width
-st.markdown("""
-<style>
-.dataframe {
-    white-space: nowrap;
-    width: 100%;
-    overflow: scroll;
-}
-</style>
-""", unsafe_allow_html=True)
+# User input for minimum distance
+min_dist = st.sidebar.number_input('Minimum Distance (in degrees):', value=3.0)
 
-st.sidebar.header('Filter by State')
-unique_states = sorted(df['STATE'].dropna().unique())
-selected_states = []
+# Filter companies based on specialization
+spec = "'EB01'"
+selected_company = itp_df.loc[itp_df['Specialization'] == spec]
 
-# Add a "Select All" checkbox in the sidebar
-select_all = st.sidebar.checkbox("Select All", True)
+# Calculate distances and filter nearby companies
+nearest_company = []
+for index, company in selected_company.iterrows():
+    xy1 = [x, y]
+    xy2 = [company['map_latitude'], company['map_longitude']]
+    distance = math.dist(xy1, xy2)
+    if distance <= min_dist:
+        temp_dict = {
+            'Company name': company['Company name'],
+            'Company address': company['Company address'],
+            'Distance from location (km)': distance * 111,
+            'Specialization': company['Specialization']
+        }
+        nearest_company.append(temp_dict)
 
-if select_all:
-    selected_states = unique_states
-    for state in unique_states:
-        checked = st.sidebar.checkbox(state, value=True, key=state)
-        if not checked:
-            select_all = False
-            selected_states.remove(state)
-else:
-    for state in unique_states:
-        if st.sidebar.checkbox(state, False, key=state):
-            selected_states.append(state)
+# Display the nearest companies in a DataFrame
+nearest_company_df = pd.DataFrame(nearest_company)
+st.write(nearest_company_df)
 
-# Text Input for Company Name Search above the table
-search_term = st.text_input("Enter Company Name:")
-
-# Sorting
-#st.markdown("### Sort by Company Name")
-sort_order = st.radio("",['Ascending', 'Descending'])
-
-# Filter by search term and selected states
-if search_term:
-    filtered_df = df[df['Company name'].str.contains(search_term, case=False, na=False)]
-else:
-    filtered_df = df
-
-filtered_df = filtered_df[filtered_df['STATE'].isin(selected_states)]
-
-# Apply sorting for the "Company name" column
-filtered_df = filtered_df.sort_values(by="Company name", ascending=(sort_order == 'Ascending'))
-
-# Define a function to create an HTML card for each company
-def create_card(row):
-    # Ensure data is a string or replace with an empty string
-    company_name = escape(str(row['Company name'])) if not pd.isna(row['Company name']) else ""
-    company_address = escape(str(row['Company address'])) if not pd.isna(row['Company address']) else ""
-    website_url = f"<a href='{escape(str(row['website_url']))}' target='_blank'>{escape(str(row['website_url']))}</a>" if not pd.isna(row['website_url']) else ""
-    company_tel = escape(str(row['Company Tel'])) if not pd.isna(row['Company Tel']) else ""
-    company_email = escape(str(row['Company Email'])) if not pd.isna(row['Company Email']) else ""
-
-    card = f"""
-    <div style="border:1px solid #eee; border-radius:5px; padding:10px; margin:5px; width: 30%; height: 300px; overflow: auto; display:inline-block; vertical-align:top">
-        <h4>{company_name}</h4>
-        <p>{company_address}</p>
-        <p>{website_url}</p>
-        <p>{company_tel}</p>
-        <p>{company_email}</p>
-    </div>
-    """
-    return card
-
-# Convert the filtered DataFrame to HTML cards
-cards = filtered_df.apply(create_card, axis=1).tolist()
-
-for i in range(0, len(cards), 3):
-    row_cards = ''.join(cards[i:i+3])
-    st.markdown(row_cards, unsafe_allow_html=True)
-
-# Run this by typing 'streamlit run app.py' in the terminal
+# Save the DataFrame to an Excel file
+if st.button('Save Nearest Companies'):
+    nearest_company_df.to_excel('nearest.xlsx', index=False)
+    st.success('Nearest companies saved to nearest.xlsx')
